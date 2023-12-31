8f073e00a131c9977d3342e1db258aa0800e6938
ansaralisahh <iansarsah@gmail.com>
2024-01-01 00:06:06 +0530

    ansar

---
+++ app/src/main/java/com/example/myapplication/jj/adMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.jj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class adMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ad_main)
+    }
+}
